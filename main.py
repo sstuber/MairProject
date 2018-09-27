cffaf2a2d09dcde02acc7d6400cb15e33ea8fe7f
Stan <stanvanmeerendonk@gmail.com>
2018-09-27 20:36:01 +0200

    Implement function to transform classification objects to numbered classification objects

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from classify_data import ClassificationData
+from classify_data import ClassificationData, ClassificationDictionary, NumberedClassification
 
 CLASSIFICATION_PATH = './classification_data.txt'
 
@@ -21,8 +21,47 @@ def get_classification_data():
     return classification_data_list
 
 
+def number_train_data(classification_dict_object: ClassificationDictionary, classification_list):
+    numbered_classification_list = []
+
+    # Transform each classification object to a numbered one
+    for classification_object in classification_list:
+        numbered_classification = NumberedClassification()
+
+        # First get the numbered speech act
+        numbered_classification.speech_act = classification_dict_object.get_speech_act_id(
+            classification_object.speech_act
+        )
+
+        # Then transform the utterance string list to a numbered string list
+        numbered_utterance_list = list(map(
+            classification_dict_object.get_training_utterance_id, classification_object.utterance
+        ))
+
+        # numbered_utterance_list = []
+        # for utterance in classification_object.utterance:
+        #     numbered_utterance = classification_dict_object.get_training_utterance_id(utterance)
+        #     numbered_utterance_list.append(numbered_utterance)
+
+        print(classification_object.utterance)
+        print(numbered_utterance_list)
+
+        numbered_classification.utterance = numbered_utterance_list
+
+        numbered_classification_list.append(numbered_classification)
+
+    return numbered_classification_list
+
+
 def main():
-    get_classification_data()
+
+    classification_list = get_classification_data()
+    classification_dict_object = ClassificationDictionary()
+    numbered_classification_list = number_train_data(classification_dict_object, classification_list)
+
+    for i in range(50):
+        item = numbered_classification_list[i]
+        print(item.utterance)
 
 
 if __name__ == "__main__":
